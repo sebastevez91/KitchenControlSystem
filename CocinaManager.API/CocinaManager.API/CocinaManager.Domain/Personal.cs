9c571e5e583ee3a3da10fe851bc97b31f34a8c0c
sebatevez91 <Sebastevez91@gmail.com>
2026-02-24 23:32:12 -0300

    initial clean architecture setup with domain models and db context

---
+++ CocinaManager.API/CocinaManager.API/CocinaManager.Domain/Personal.cs
@@ -0,0 +1,18 @@
+﻿namespace CocinaManager.API.CocinaManager.Domain;
+
+public class Personal
+{
+    public Guid Id { get; set; }
+    public string Nombre { get; set; }
+    public string Documento { get; set; }
+    public string Cargo { get; set; }
+    public EstadoPersonal Estado { get; set; }
+}
+
+public enum EstadoPersonal
+{
+    Activo,
+    Enfermo,
+    Licencia,
+    Ausente
+}
---
+++ CocinaManager.API/CocinaManager.API/Program.cs
@@ -0,0 +1,30 @@
+using CocinaManager.API.CocinaManager.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Conexión a BD
+builder.Services.AddDbContext<CocinaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
